e85abdeeb0e257c0704b12308deb117a582babcd
zijievv <zijie_vv@icloud.com>
2020-07-18 16:01:53 +0800

    Added enum AppAction.

---
+++ PokeMaster/State/AppAction.swift
@@ -0,0 +1,13 @@
+//
+//  AppAction.swift
+//  PokeMaster
+//
+//  Created by zijie vv on 2020-07-18.
+//  Copyright © 2020 OneV's Den. All rights reserved.
+//
+
+import Foundation
+
+enum AppAction {
+  case login(email: String, password: String)
+}
